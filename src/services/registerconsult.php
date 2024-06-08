1335ceced4f7d7224a6deef1028b6bc92f9db0d3
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-06-08 15:52:56 -0300

    upgrade register consulta doutorX

---
+++ src/services/registerconsult.php
@@ -0,0 +1,65 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once 'pdoconnection.php';
+
+$response_json = file_get_contents("php://input");
+$dados = json_decode($response_json, true);
+
+if ($dados) {
+
+    $query_checked_cpf = "SELECT COUNT(*) FROM cpf WHERE cpf = :cpf";
+    $checked_cpf = $conn->prepare($query_checked_cpf);
+    $checked_cpf->bindParam(':cpf', $dados['cpf'], PDO::PARAM_STR);
+    $checked_cpf->execute();
+    $cpf_count = $checked_cpf->fetchColumn();
+    if ($cpf_count === 0) {
+        $query_addcpf = "INSERT INTO cpf (cpf, name) VALUES (:cpf, :name)";
+        $cad_cpf = $conn->prepare($query_addcpf);
+        $cad_cpf->bindParam(':cpf', $dados['cpf'], PDO::PARAM_STR);
+        $cad_cpf->bindParam(':name', $dados['name'], PDO::PARAM_STR);
+        $cad_cpf->execute();
+
+        $query_checked_telephone = "SELECT COUNT(*) FROM telephone WHERE telephone = :telephone";
+        $checked_telephone = $conn->prepare($query_checked_telephone);
+        $checked_telephone->bindParam(':telephone', $dados['telephone'], PDO::PARAM_STR);
+        $checked_telephone->execute();
+        $telephone_count = $checked_telephone->fetchColumn();
+        if ($telephone_count === 0) {
+            $query_addtelephone = "INSERT INTO telephone (telephone) VALUES (:telephone)";
+            $cad_telephone = $conn->prepare($query_addtelephone);
+            $cad_telephone->bindParam(':telephone', $dados['telephone'], PDO::PARAM_STR);
+            $cad_telephone->execute();
+        };
+        function checkeZipCodeAdd($dados, $conn)
+        {
+            $query_checked_zipcode = "SELECT COUNT(*) FROM zipcode WHERE zipcode = :zipcode";
+            $checked_zipcode = $conn->prepare($query_checked_zipcode);
+            $checked_zipcode->bindParam(':zipcode', $dados['zipcode'], PDO::PARAM_STR);
+            $checked_zipcode->execute();
+            $zipcode_count = $checked_zipcode->fetchColumn();
+            if ($zipcode_count === 0) {
+                $query_addzipcode = "INSERT INTO zipcode (zipcode, street, district, city) VALUES (:zipcode, :street, :district, :city)";
+                $cad_zipcode = $conn->prepare($query_addzipcode);
+                $cad_zipcode->bindParam(':zipcode', $dados['zipcode'], PDO::PARAM_STR);
+                $cad_zipcode->bindParam(':street', $dados['street'], PDO::PARAM_STR);
+                $cad_zipcode->bindParam(':district', $dados['district'], PDO::PARAM_STR);
+                $cad_zipcode->bindParam(':city', $dados['city'], PDO::PARAM_STR);
+                $cad_zipcode->execute();
+            };
+            $query_addaddress_all = "INSERT INTO address_all (zipcode, residencenumber, building, buildingblock, apartment) VALUES (:zipcode, :residencenumber, :building, :buildingblock, :apartment)";
+            $cad_addaddress_all = $conn->prepare($query_addaddress_all);
+            $cad_addaddress_all->bindParam(':zipcode', $dados['zipcode'], PDO::PARAM_STR);
+            $cad_addaddress_all->bindParam(':residencenumber', $dados['residencenumber'], PDO::PARAM_STR);
+            $cad_addaddress_all->bindParam(':building', $dados['building'], PDO::PARAM_STR);
+            $cad_addaddress_all->bindParam(':buildingblock', $dados['buildingblock'], PDO::PARAM_STR);
+            $cad_addaddress_all->bindParam(':apartment', $dados['apartment'], PDO::PARAM_STR);
+            $cad_addaddress_all->execute();
+            if ($cad_addaddress_all->rowCount()) {
+                $_SESSION['address_id'] = $conn->lastInsertId();
+            };
+        }
+    };
+};
