f468c677066be90161e43467a777f1cb4ad10844
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-06-12 11:30:38 -0300

    upgrade get dados banco

---
+++ src/services/getconsults.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once 'pdoconnection.php';
+
+$query_get_consult = "SELECT consultation_id, consultation.cpf, name, consultation.crm, plan, consultdatestart FROM consultation JOIN cpf on consultation.cpf = cpf.cpf JOIN crm on consultation.crm = 5001";
+$result_consultations = $conn->prepare($query_get_consult);
+$result_consultations->execute();
+
+if (($result_consultations) && ($result_consultations->rowCount() != 0)) {
+    while ($row_consultation = $result_consultations->fetch(PDO::FETCH_ASSOC)) {
+        extract($row_consultation);
+        $list_consults['rescods'][$consultation_id] = [
+            'consultation_id' => $consultation_id,
+            'crm' => $crm,
+            'cpf' => $cpf,
+            'name' => $name,
+            'plan' => $plan,
+            'consultdatestart' => $consultdatestart
+        ];
+    };
+    http_response_code(200);
+    echo json_encode($list_consults);
+};
