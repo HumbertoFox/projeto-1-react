41c9841de54fffc86abecf0037624c106a0c32d3
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-06-19 14:27:58 -0300

    git ignore ajuste vite.config

--- src/services/connection.ts
+++
@@ -1,5 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-const pool = new Pool({
-    connectionString: "postgres://default:mpKCr3Vsqvi9@ep-white-sea-a4bono27-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
-});
