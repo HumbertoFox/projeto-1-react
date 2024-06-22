72c25e2954cbb03ebe4f3a72c38352a48ab08a53
Humberto Ribeiro <humbertofox@hotmail.com>
2024-06-22 20:55:43 -0300

    consfig vit config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({
-  plugins: [react()],
+import dotenvExpand from 'dotenv-expand';
+import { loadEnv, defineConfig } from 'vite';
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand.expand({ parsed: env });
+  };
+  return {};
 });
