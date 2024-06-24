85b33170e7fb05fc2441d1a65d58f8fc063d96a8
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-06-24 19:51:27 -0300

    update tsx ts

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import dotenvExpand from 'dotenv-expand';
 import { loadEnv, defineConfig } from 'vite';
+import { react } from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   if (mode === 'development') {
     const env = loadEnv(mode, process.cwd(), '');
     dotenvExpand.expand({ parsed: env });
   };
-  return {};
+  return {
+    plugins: [react()],
+    resolve: {
+      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    }
+  };
 });
