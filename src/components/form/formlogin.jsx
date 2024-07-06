b599dd404648a3d0ace9358417ef49fbdd3922ee
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-07-06 03:02:51 -0300

    update login redirect agenda

--- src/components/form/formlogin.jsx
+++ src/components/form/formlogin.jsx
@@ -44,7 +44,7 @@ export const FormLogin = () => {
                 login(response.user);
                 setTimeout(() => {
                     if (buttonType == "enter") {
-                        navigate("/");
+                        navigate("/agenda");
                     } else if (buttonType == "menu") {
                         navigate("/menuRegister");
                     };
