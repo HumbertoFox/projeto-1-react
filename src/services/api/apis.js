d56ae9784401d370e81b6918c03093b08fec2d05
Humberto Ribeiro <humbertofox@hotmail.com>
2024-07-24 08:04:39 -0300

    update api vite json

--- src/services/api/apis.js
+++ src/services/api/apis.js
@@ -1,6 +1,8 @@
+const PORT = process.env.PORT || 3000;
+
 const apiDbPostgres = async (data, rota) => {
     if (rota === 'registeruser' || rota === 'registerconsultation' || rota === 'loginuser' || rota === 'registerdoctor' || rota === 'searchpatient' || rota === 'searchuser') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`http://localhost:${PORT}/${rota}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -9,7 +11,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'edituser' || rota === 'editpatient') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`http://localhost:${PORT}/${rota}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,7 +20,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'eventspatient' || rota === 'eventsconsultsy' || rota === 'eventsconsultsx') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`http://localhost:${PORT}/${rota}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -27,7 +29,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'removeuser') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`http://localhost:${PORT}/${rota}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 import dotenvExpand from 'dotenv-expand';
 import { loadEnv, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ mode }) => {
-  if (typeof process !== 'undefined' || mode === 'development') {
-    const env = loadEnv(mode, process.cwd(), '');
-    dotenvExpand.expand({ parsed: env });
-  };
-  const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-  };
+
+  const env = loadEnv(mode, process.cwd(), '');
+  dotenvExpand.expand({ parsed: env });
+
+  const envWithProcessPrefix = Object.keys(env).reduce((prev, curr) => {
+    prev[`process.env.${curr}`] = JSON.stringify(env[curr]);
+    return prev;
+  }, {});
+
   return {
     plugins: [react()],
-    define: {
-      'process.env': {
-        REACT_APP_API_KEY: JSON.stringify(config.apiKey)
-      }
-    },
+    define: envWithProcessPrefix,
     resolve: {
-      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+      extensions: ['.js', '.ts', '.jsx', '.tsx']
     }
   };
 });
