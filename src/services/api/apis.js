00be929a252d0f3354dd1d599ad7f1d484848410
Humberto Ribeiro <humbertofox@hotmail.com>
2024-07-24 21:24:18 -0300

    update package.json e API PORT e URL

--- src/services/api/apis.js
+++ src/services/api/apis.js
@@ -1,6 +1,9 @@
+const URL = process.env.URL_FETCH || "http://localhost";
+const PORT = process.env.PORT || 3000;
+
 const apiDbPostgres = async (data, rota) => {
     if (rota === 'registeruser' || rota === 'registerconsultation' || rota === 'loginuser' || rota === 'registerdoctor' || rota === 'searchpatient' || rota === 'searchuser') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`${URL}:${PORT}/${rota}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -9,7 +12,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'edituser' || rota === 'editpatient') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`${URL}:${PORT}/${rota}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,7 +21,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'eventspatient' || rota === 'eventsconsultsy' || rota === 'eventsconsultsx') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`${URL}:${PORT}/${rota}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -27,7 +30,7 @@ const apiDbPostgres = async (data, rota) => {
         });
         return response.json();
     } else if (rota === 'removeuser') {
-        const response = await fetch(`/${rota}`, {
+        const response = await fetch(`${URL}:${PORT}/${rota}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
