ba07a58f1d983db7a49a39e048c37b9d6c522455
Humberto Ribeiro <humbertoemaus@gmail.com>
2024-07-31 05:43:54 -0300

    update vite vercel

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import dotenvExpand from 'dotenv-expand';
 import { loadEnv, defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig(({ mode }) => {
-
   const env = loadEnv(mode, process.cwd(), '');
   dotenvExpand.expand({ parsed: env });
 
   const envWithProcessPrefix = Object.keys(env).reduce((prev, curr) => {
-    prev[`process.env.${curr}`] = JSON.stringify(env[curr]);
+    const validKey = curr.replace(/[^\w]/g, '_');
+    prev[`process.env.${validKey}`] = JSON.stringify(env[curr]);
     return prev;
   }, {});
 
   return {
-    plugins: [react()],
+    plugins: [vercel()],
     define: envWithProcessPrefix,
     resolve: {
       extensions: ['.js', '.ts', '.jsx', '.tsx']
